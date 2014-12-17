c89ee39dc4ee627365a750fef3bb45b962a655fb
Nick <nickmyself@hotmail.com>
2014-12-17 17:24:21 -0500

    大改前

---
+++ final/MovieWeb/SearchResult/login_jz337.php
@@ -0,0 +1,13 @@
+<?php // login_yourlastname.php   make sure to place in personal folder
+
+$db_hostname = 'studentweb.comminfo.rutgers.edu';
+$db_database = 'class-2014-1-17-610-557-01_jz337';
+$db_username = 'jz337';
+$db_password = 'oiC#V!5zenha';
+/*
+$db_hostname = 'localhost';
+$db_database = 'class-2014-1-17-610-557-01_jz337';
+$db_username = 'root';
+$db_password = 'iamnick42';
+*/
+?>
